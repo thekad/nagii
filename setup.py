a97d34522189539c6c07bf8e993c02dc9b7af083
Jorge Gallegos <kad@eventbrite.com>
2011-04-27 20:57:37 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# -*- mode: python; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
+
+import setuptools
+
+from src import nagii
+
+setuptools.setup(
+    name = 'nagii',
+    version = nagii.VERSION,
+    author = 'Jorge Gallegos',
+    author_email = 'kad@blegh.net',
+    description = 'A straightforward Nagios modelling library',
+    install_requires = [
+        'mako',
+    ],
+    packages = setuptools.find_packages('src'),
+    package_dir = { '': 'src' },
+    zip_safe = False,
+)
