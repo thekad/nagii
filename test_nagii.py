e0781ddfcbce4cdaed3613a17b82fdd3c558001b
Jorge Gallegos <kad@eventbrite.com>
2011-04-27 22:32:58 -0700

    A beefier example

--- test_nagii.py
+++ test_nagii.py
@@ -3,19 +3,29 @@
 # -*- mode: python; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
 # vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
 
+from mako import template as mako_template
+
 import nagii
 
-template = nagii.NagiosTemplate(name="generic-host",
-    address="10.1.100.1", max_check_attempts=1, check_period=5,
+tpl = """
+define ${obj._type} {
+%   for k, v in obj._public().items():
+    ${k}             ${v}
+%   endfor
+}
+"""
+
+host_template = nagii.NagiosTemplate(name="generic-host",
+    max_check_attempts=1, check_period=5,
     contact_groups="nagiosadmin", notification_interval=5,
     notification_period=960)
-template._set_type('host')
+host_template._set_type('host')
 
-host = nagii.Host(template, host_name="foo_host", alias="Stoopid")
+hosts = []
 
-print template.max_check_attempts
-print host.alias
-print host.contact_groups
-print template._public()
-print host._public()
+for i in xrange(1, 10):
+    hosts.append(nagii.Host(host_template, host_name="host_%d" % i, alias="Host number %d" % i, address="10.1.100.%d" % i))
 
+tpl = mako_template.Template(tpl)
+for x in [ host_template ] + hosts:
+    print tpl.render(obj=x)
