bc4699df0317b9fb731688abbc6b5c2b2c84c120
Jorge Gallegos <kad@eventbrite.com>
2011-04-27 20:49:55 -0700

    Adding VERSION

--- nagii/__init__.py
+++ nagii/__init__.py
@@ -3,6 +3,8 @@
 # -*- mode: python; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
 # vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
 
+VERSION='0.1'
+
 
 class NagiosObject(object):
     """
