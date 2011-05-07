4e1db15bed828919cfbf9f5e37f338bc7a70b9a0
Mike Newton <mike@delusion.org>
2011-05-07 07:55:09 -0700

    move register setting to template constructor

--- src/nagii/__init__.py
+++ src/nagii/__init__.py
@@ -74,9 +74,6 @@ class NagiosObject(object):
             elif _.startswith('_custom'):
                 public.append((_[7:], getattr(self, _)))
 
-        if isinstance(self,NagiosTemplate):
-            public.append(("register",0))
-
         return dict(public)
 
     def _get_required(self):
@@ -130,6 +127,7 @@ class NagiosTemplate(NagiosObject):
 
     def __init__(self, *args, **kwargs):
         NagiosObject.__init__(self, *args, **kwargs)
+        setattr(self, "register", 0)
 
     def _set_type(self, stype):
         self._type = stype
